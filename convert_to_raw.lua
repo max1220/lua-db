adaa6163e5c788216a0ac3e7dbe8c20924ef0ea7
max1220 <max1220@users.noreply.github.com>
2019-08-27 15:05:53 +0200

    Added optional 3rd argument for output in rgb/rgba

--- convert_to_raw.lua
+++ convert_to_raw.lua
@@ -1,4 +1,12 @@
 #!/usr/bin/env luajit
 local ldb = require("lua-db")
 local db = ldb.imlib.from_file(assert(arg[1], "Argument 1 is input file"))
-assert(io.open(assert(arg[2], "Argument 2 is output file"), "wb")):write(db:dump_data())
+local file = assert(io.open(assert(arg[2], "Argument 2 is output file"), "wb"), "Can't open file!")
+if (not arg[3]) or (arg[3] == "rgba") then
+	file:write(db:dump_data_rgba())
+elseif arg[3] == "rgb" then
+	file:write(db:dump_data_rgb())
+else
+	print("Unknown pixel format! Try rgba, rgb or blank for default(rgba)")
+end
+file:close()
