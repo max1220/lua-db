aeb242cf76cdffd74e4157ee5a1da1710085781e
max1220 <max1220@users.noreply.github.com>
2020-07-13 21:26:55 +0200

    document_tree.lua describes the structure of exported documentation

---
+++ doc/document_tree.lua
@@ -0,0 +1,36 @@
+return {
+	{
+		title = "About",
+		file = "about.md",
+	},
+	{
+		title = "Installation",
+		file = "installation.md",
+		children = {
+			{
+				title = "Dependencies",
+				file = "dependencies.md",
+			},
+			{
+				title = "Building Documentation",
+				file = "documentation_build.md",
+			},
+			{
+				title = "Development Installation",
+				file = "installing_symlinks.md",
+			},
+		}
+	},
+	{
+		title = "Basic Usage",
+		file = "basic_usage.md",
+	},
+	{
+		title = "Examples",
+		file = "examples.md",
+	},
+	{
+		title = "Development",
+		file = "development.md",
+	},
+}
