c25d013c0884d4e89da87766f958f736b977f36b
Max <max@debian-fx>
2020-10-12 13:27:55 +0200

    Initial implementation of terminal render overhaul

--- lua/terminal.lua
+++ lua/terminal.lua
@@ -4,7 +4,7 @@ Utillities for working with ANSI terminal esacape sequences.
 
 Exports a single function:
 
-	term = terminal.new_terminal(write_cb, read_cb, terminal_type)
+	term = terminal.new_terminal(write_cb, read_cb, terminal_type, supports_unicode)
 
 	write_cb(self, str)
 
@@ -35,36 +35,72 @@ term:set_bg_color(r,g,b)
 
 
 
-function terminal.new_terminal(write_cb, read_cb, term_type)
+
+
+function terminal.new_terminal(write_cb, read_cb, term_type, supports_unicode)
 	local term = {}
 	assert(type(write_cb)=="function")
+
+	-- append drawbuffer drawing routines(:drawbuffer_colors(), :drawbuffer_characters(), etc.)
+	local terminal_drawbuffer = require("lua-db.terminal_drawbuffer")
+	terminal_drawbuffer(term)
 
 	term.write_cb = write_cb -- used to write to the terminal(emulator).
 	term.read_cb = read_cb -- used to read from the terminal.
 	term.palettes = require("lua-db.terminal_palettes") -- list of escape codes and r,g,b values. Used for setting colors.
-	term.term_type = term_type or "ansi" -- term_types = {"linux", "ansi_3bit", "ansi_4bit", "ansi_8bit", "ansi_24bit"}
+	term.term_type = term_type or "ansi_3bit" -- term_types = "linux", "ansi_3bit", "ansi_4bit", "ansi_8bit", "ansi_24bit"
+	term.supports_unicode = term_type ~= "linux"
+	term.no_color = false
+	if supports_unicode ~= nil then
+		term.supports_unicode = supports_unicode
+	end
+
+	term.read_buf = {} -- the buffer of last received characters
+	term.read_buf_len = 16 -- buffer length
+	function term:read() -- read using read_cb if available(returns last character buffer, for easy pattern matching)
+		if not term.read_cb then
+			return
+		end
+		local str = self:read_cb() -- should be a string of length 1
+		if str then
+			table.insert(self.read_buf, str)
+			if #self.read_buf>self.read_buf_len then
+				table.remove(self.read_buf, 1) -- keep the buffer at read_buf_len entries
+			end
+		end
+		return table.concat(self.read_buf) -- always return the complete buffer
+	end
 
 	-- set cursor to x,y
 	function term:set_cursor(_x,_y)
 		local x = math.floor(tonumber(_x) or 0) + 1
 		local y = math.floor(tonumber(_y) or 0) + 1
-		self:write_cb(("\027[%d;%dH"):format(y,x))
+		return self:write_cb(("\027[%d;%dH"):format(y,x))
+	end
+
+	-- set cursor to top-left
+	function term:reset_cursor()
+		return self:write_cb("\027[;H")
 	end
 
 	-- get the cursor position. Only possible if the read_cb is provided, since
 	-- the actual terminal(emulator) needs to answer.
 	function term:get_cursor()
-		if self.read_cb then
-			self:write_cb("\027[6n")
-			local input = self:read_cb()
-			while not input do
-				input = self:read_cb()
-			end
-			local y,x = input:match("\027[(%d+);(%d+)R")
-			if y and x then
-				return x,y
-			end
+		self:write_cb("\027[6n")
+		-- self:read() returns nil if read is not available, a string otherwise, even if read_cb() returned nil.
+		-- Reads up to one character per call(might be zero if read_cb works "non-blocking", returning nil for some calls).
+		local input_buf = self:read()
+		if input_buf == nil then
+			return -- read_cb not available
 		end
+		for _=1, self.read_buf_len do -- need to fill :read() internal buffer
+			local y,x = input_buf:match("\027[(%d+);(%d+)R$") -- match at end to only call per occurance in the buffer
+			if y and x then
+				return x,y -- found a valid sequence, return cursor coordinates as repored by terminal
+			end
+			input_buf = self:read() -- atempt again after reading another character
+		end
+		-- pattern not found in input from read_cb after enough atempts
 	end
 
 	-- get the terminal size by ANSI escape sequences
@@ -76,14 +112,43 @@ function terminal.new_terminal(write_cb, read_cb, term_type)
 		return w,h
 	end
 
+	function term:alternate_screen_buffer(enabled)
+		if enabled then
+			return self:write_cb("\027[?1049h") -- Enable alternative screen buffer
+		else
+			return self:write_cb("\027[?1049l") -- Disable alternative screen buffer
+		end
+	end
+
+	function term:mouse_tracking(enabled)
+		if enabled then -- see xterm ctlseqs
+			self:write_cb("\027[?1000h") -- enable X11-style mouse tracking(report mouse button press and mouse button release)
+			self:write_cb("\027[?1003h") -- enable all mouse movement reports
+			self:write_cb("\027[?1006h") -- report as decimal for xterm-like
+			self:write_cb("\027[?1015h") -- report as decimal for urxvt
+		else
+			self:write_cb("\027[?1000l") -- disable all mouse tracking
+			self:write_cb("\027[?1003l")
+			self:write_cb("\027[?1006l")
+			self:write_cb("\027[?1015l")
+		end
+	end
+
 	-- clear the screen
 	function term:clear_screen()
-		self:write_cb("\027[2J"..self:set_cursor(0,0))
+		return self:write_cb("\027[2J"..self:set_cursor(0,0))
+	end
+
+	-- reset the SGR parameters, clear the screen, and set cursor to top left.
+	function term:reset_all()
+		self:reset_color()
+		self:clear_screen()
+		self:reset_cursor()
 	end
 
 	-- reset the SGR attributes(fg/bg color, bold, ...)
 	function term:reset_color()
-		self:write_cb("\027[0m")
+		return self:write_cb("\027[0m")
 	end
 
 	-- check r,g,b values
@@ -111,75 +176,89 @@ function terminal.new_terminal(write_cb, read_cb, term_type)
 	end
 
 	-- write the terminal escape sequence
-	function term:set_fg_color_ansi_24bit(_r, _g, _b)
-		local r,g,b = check_color(_r,_g,_b)
-		return self:write_cb("\027[38;2;" .. r .. ";" .. g .. ";" .. b .. "m")
+	function term:fg_color_ansi_24bit(r,g,b)
+		r,g,b = check_color(r,g,b)
+		return "\027[38;2;" .. r .. ";" .. g .. ";" .. b .. "m"
 	end
-	function term:set_fg_color_ansi_8bit(r,g,b)
-		return self:write_cb(get_palette_entry_for_color(self.palettes.fg_palette_8bit, r,g,b).code)
+	function term:fg_color_ansi_8bit(r,g,b)
+		return get_palette_entry_for_color(self.palettes.fg_palette_8bit, r,g,b).code
 	end
-	function term:set_fg_color_ansi_4bit(r,g,b)
-		return self:write_cb(get_palette_entry_for_color(self.palettes.fg_palette_4bit, r,g,b).code)
+	function term:fg_color_ansi_4bit(r,g,b)
+		return get_palette_entry_for_color(self.palettes.fg_palette_4bit, r,g,b).code
 	end
-	function term:set_fg_color_ansi_3bit(r,g,b)
-		return self:write_cb(get_palette_entry_for_color(self.palettes.fg_palette_3bit, r,g,b).code)
+	function term:fg_color_ansi_3bit(r,g,b)
+		return get_palette_entry_for_color(self.palettes.fg_palette_3bit, r,g,b).code
 	end
-	function term:set_bg_color_ansi_24bit(_r, _g, _b)
-		local r,g,b = check_color(_r,_g,_b)
-		return self:write_cb("\027[48;2;" .. r .. ";" .. g .. ";" .. b .. "m")
+	function term:bg_color_ansi_24bit(r,g,b)
+		r,g,b = check_color(r,g,b)
+		return "\027[48;2;" .. r .. ";" .. g .. ";" .. b .. "m"
 	end
-	function term:set_bg_color_ansi_8bit(r,g,b)
-		return self:write_cb(get_palette_entry_for_color(self.palettes.bg_palette_8bit, r,g,b).code)
+	function term:bg_color_ansi_8bit(r,g,b)
+		return get_palette_entry_for_color(self.palettes.bg_palette_8bit, r,g,b).code
 	end
-	function term:set_bg_color_ansi_4bit(r,g,b)
-		return self:write_cb(get_palette_entry_for_color(self.palettes.bg_palette_4bit, r,g,b).code)
+	function term:bg_color_ansi_4bit(r,g,b)
+		return get_palette_entry_for_color(self.palettes.bg_palette_4bit, r,g,b).code
 	end
-	function term:set_bg_color_ansi_3bit(r,g,b)
-		return self:write_cb(get_palette_entry_for_color(self.palettes.bg_palette_3bit, r,g,b).code)
+	function term:bg_color_ansi_3bit(r,g,b)
+		return get_palette_entry_for_color(self.palettes.bg_palette_3bit, r,g,b).code
 	end
 
 	-- set the terminal foreground color(automatic terminal type)
-	function term:set_fg_color(r,g,b)
+	function term:fg_color(r,g,b)
+		if self.no_color then
+			return ""
+		end
 		if self.term_type == "ansi_24bit" then
 			-- use the 24-bit colors
-			return self:set_fg_color_ansi_24bit(r,g,b)
+			return self:fg_color_ansi_24bit(r,g,b)
 		elseif self.term_type == "ansi_8bit" then
 			-- use the 216 colors + 24 grey level ansi escape sequences
-			return self:set_fg_color_ansi_8bit(r,g,b)
+			return self:fg_color_ansi_8bit(r,g,b)
 		elseif self.term_type == "ansi_4bit" then
 			-- use the extended 16 ANSI colors(8 normal + 8 bright)
-			return self:write_cb(get_palette_entry_for_color(self.palettes.fg_palette_4bit).code)
+			return self:fg_color_ansi_4bit(r,g,b)
 		elseif self.term_type == "ansi_3bit" then
 			-- use the basic 8 ANSI colors
-			return self:write_cb(get_palette_entry_for_color(self.palettes.fg_palette_3bit).code)
+			return self:fg_color_ansi_3bit(r,g,b)
 		elseif self.term_type == "linux" then
 			-- linux supports the 4bit ANSI foreground color
-			return self:write_cb(get_palette_entry_for_color(self.palettes.fg_palette_4bit).code)
+			return self:fg_color_ansi_4bit(r,g,b)
 		end
 	end
 
 	-- set the terminal background color(automatic terminal type)
-	function term:set_bg_color(r,g,b)
+	function term:bg_color(r,g,b)
+		if self.no_color then
+			return ""
+		end
 		if self.term_type == "ansi_24bit" then
 			-- use the 24-bit colors
-			return self:set_fg_color_ansi_24bit(r,g,b)
+			return self:bg_color_ansi_24bit(r,g,b)
 		elseif self.term_type == "ansi_8bit" then
 			-- use the 216 colors + 24 grey level ansi escape sequences
-			return self:set_fg_color_ansi_8bit(r,g,b)
+			return self:bg_color_ansi_8bit(r,g,b)
 		elseif self.term_type == "ansi_4bit" then
 			-- use the extended 16 ANSI colors(8 normal + 8 bright)
-			return self:write_cb(get_palette_entry_for_color(self.palettes.fg_palette_4bit).code)
+			return self:bg_color_ansi_4bit(r,g,b)
 		elseif self.term_type == "ansi_3bit" then
 			-- use the basic 8 ANSI colors
-			return self:write_cb(get_palette_entry_for_color(self.palettes.fg_palette_3bit).code)
+			return self:bg_color_ansi_3bit(r,g,b)
 		elseif self.term_type == "linux" then
 			-- linux supports the 4bit ANSI foreground color
-			return self:write_cb(get_palette_entry_for_color(self.palettes.fg_palette_4bit).code)
+			return self:bg_color_ansi_4bit(r,g,b)
 		end
 	end
 
-	-- draws a vertical percentage bar using unicode block characters. Len is the length in characters, pct the percentage(0-1)
-	function term:draw_pct_bar(len, pct)
+	function term:set_fg_color(r,g,b)
+		return self:write_cb(self:fg_color(r,g,b))
+	end
+
+	function term:set_bg_color(r,g,b)
+		return self:write_cb(self:bg_color(r,g,b))
+	end
+
+	-- draws a vertical percentage bar using unicode block characters. Len is length in characters, pct the percentage(0-1)
+	function term:draw_pct_bar_unicode(len, pct)
 		local blocks = {
 			string.char(0xE2,0x96,0x8F),
 			string.char(0xE2,0x96,0x8E),
@@ -207,6 +286,29 @@ function terminal.new_terminal(write_cb, read_cb, term_type)
 		return table.concat(str)
 	end
 
+	-- draw a percentage bar using only 7bit ASCII characters.
+	function term:draw_pct_bar_ascii(len, pct)
+		local str = {}
+		table.insert(str, "[")
+		for i=1, len-2 do
+			local cpct = (i-1)/(len-3)
+			if cpct<=pct then
+				table.insert(str, "#")
+			else
+				table.insert(str, " ")
+			end
+		end
+		table.insert(str, "]")
+		return table.concat(str)
+	end
+
+	function term:draw_pct_bar(len, pct)
+		if self.supports_unicode then
+			return term:draw_pct_bar_unicode(len, pct)
+		else
+			return term:draw_pct_bar_ascii(len, pct)
+		end
+	end
 
 	return term
 end
